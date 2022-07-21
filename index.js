584054a6f5b5b7e7db3407d63ba18bac6338954b
codex12345 <rofakos22@gmail.com>
2022-07-21 05:55:49 -0700

    added code

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hello")
+let a= "soma"
+let c= "name"
+console.log(a)
+let b= "a" + "name"
+let number= 2 + 2
+console.log(number)
+let x = 3
+let y = 4
+let multiply = x*y
+console.log(multiply)
+let subtract = x-y
+console.log (subtract)
+let divide = x/y
+console.log(divide)
+let add = x+y
+console.log(add) 
+let equals = x==y
+console.log(equals)
